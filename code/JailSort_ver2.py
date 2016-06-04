780fbde14bdeb37ed751b3b5415b658b0d406eff
mjturtora <mjturtora@gmail.com>
2016-06-04 01:20:29 -0400

    Removed sq brackets to try dicts and shortened samplecsv.csv to ease testing.

--- code/JailSort_ver2.py
+++ code/JailSort_ver2.py
@@ -42,7 +42,8 @@ inmateTable = {
     'POB': ''}
 
 # Holds data for the Arrest table in the MySQL db
-arrestTable = [{
+# MT removed brackets
+arrestTable = {
     'BookingNum': '',
     'ArrestDate': '',
     'BookingDate': '',
@@ -51,24 +52,26 @@ arrestTable = [{
     'RelRemarks': '',
     'ABN': '',
     'SOID': '',
-    'Agency': ''}]
+    'Agency': ''}
 
-# Holds data for the CourtCase table in the MySQL db	
-courtcaseTable = [{
+# Holds data for the CourtCase table in the MySQL db
+# MT remove brackets
+
+courtcaseTable = {
     'CaseNum': '',
     'CourtCode': '',
     'SOID': '',
-    'BookingNum': ''}]
+    'BookingNum': ''}
 
 # Holds data for the Charge table in the MySQL db
-chargeTable = [{
+chargeTable = {
     'CaseNum': '',
     'CourtCode': '',
     'SOID': '',
     'Type': '',
     'Desc': '',
     'Counts': ''
-}]
+}
 
 
 # Slices the data from LayoutB into individual entries and stores them in entryBlocks
@@ -95,7 +98,8 @@ def block_maker(file):
     # Enters data from an entry in entryBlocks to table dictionaries
     for entry in entryBlocks:
         for line in entry:
-            # All first lines of an entry have the same format. This pulls the data based on that format.
+            # All first lines of an entry have the same format. This pulls the
+            # data based on that format.
             if line == entry[0]:
                 line = str(line)
                 line = line.split(',')
@@ -105,7 +109,8 @@ def block_maker(file):
                     # arrestTable['Agency'] = line[5]
                     # arrestTable['ABN'] = line[6]
 
-            # All second lines of an entry have the same format. This pulls the data based on that format.
+            # All second lines of an entry have the same format. This pulls the
+            # data based on that format.
             if line == entry[1]:
                 line = str(line)
                 line = line.split(',')
@@ -117,14 +122,16 @@ def block_maker(file):
                 inmateTable['DOB'] = slash.join(line[5], line[3], line[4])
             # Figure out how to properly check that charges aren't repeated.
             # Count attribute is in chargeTable for multiples of same charge.
-            # ?Make tempCharge list to hold them, check against held charge, aggregate for counts?
+            # ?Make tempCharge list to hold them, check against held charge,
+            # aggregate for counts?
             # chargeTable['Type'] = line[6]
             # chargeTable['Desc'] = line[7]
             # chargeTable['Court'] = line[8]
             # chargeTable['CaseNum'] = line[9]
 
             # This should find the lines that are only charge entries, if they exist.
-            # This should skip the second line of an entry even though it has a charge type.
+            # This should skip the second line of an entry even though it has a
+            # charge type.
             # if any(charge in chargeTypes for line in entry[x > 1]):
             # line = str(line)
             # line = line.split(',')
@@ -133,7 +140,8 @@ def block_maker(file):
             # chargeTable['Court'] = line[3]
             # chargeTable['CaseNum'] = line[4]
 
-            # ADDRESS will always be in second to last line of an entry. This pulls the data based on that format.
+            # ADDRESS will always be in second to last line of an entry. This pulls the
+            # data based on that format.
             if 'ADDRESS' in line:
                 line = str(line)
                 line = line.split(',')
@@ -143,19 +151,23 @@ def block_maker(file):
                 inmateTable['City'] = line[2]
                 inmateTable['POB'] = line[4]
 
-            # SOID is always in last line of an entry. This pulls the data based on that format.
+            # SOID is always in last line of an entry. This pulls the data based on
+            # that format.
             if 'SOID' in str(line):
                 line = str(line)
                 line = line.split(',')
-                # Handles formatting so the title for ReleaseDate, ReleaseCode, and SOID aren't included.
+                # Handles formatting so the title for ReleaseDate, ReleaseCode, and
+                # SOID aren't included.
                 line = line.split(':')
                 # arrestTable['ReleaseDate'] = line[1]
                 # arrestTable['ReleaseCode'] = line[3]
                 inmateTable['SOID'] = line[5]
 
-            # Will take the data from the dictionaries and enter them into the JailPop database
+            # Will take the data from the dictionaries and enter them into the
+                # JailPop database
             # Still need to work out a looping feature to execute the queries
-            # Also, need to include a checking feature that will ensure there aren't redundant entries
+            # Also, need to include a checking feature that will ensure there aren't
+                # redundant entries
 
             # cursor = db.cursor()
 
