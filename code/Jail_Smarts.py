5e3738c58f70c0057a061047f1a2de15751e6847
mjturtora <mjturtora@gmail.com>
2016-07-10 04:17:15 -0400

    Jail_Smarts.py authenticated SmartyStreets address lookup

--- code/Jail_Smarts.py
+++ code/Jail_Smarts.py
@@ -2,9 +2,26 @@
 # Program: Jail_Smarts.py
 # Decription: Script will pull data from Jail Population text files and hit
 # SmartyStreets API for Address cleanup
+
+
+"""
+Watch the encoding!
+https://smartystreets.com/docs/us-street-api
+A common mistake we see is a non-encoded pound sign (#) like in an apartment
+number (# 409). This character, when properly encoded in a URL, becomes %23.
+
+A POST request allows a larger volume of data (MAX: 100 addresses or 32K per request)
+to be sent in the HTTP Request Body. In this case, the data should be encoded as a
+JSON array where each element in the array is a JSON object with field names
+identical to those in the field listing below. Here's a sample request with two
+addresses being sent...
+"""
 # Version: 0.1
 
 import csv
+import json
+import pprint
+import urllib
 
 # Array that holds the all the sliced out blocks
 entry_blocks = []
@@ -34,6 +51,59 @@ arrest_table = {
 
 # Initialize charge dict
 charge_table = {}
+
+# use Smarty Streets to normalize addresses
+def read_auth():
+    print 'READ AUTH'
+    lines =[]
+    with open('../data/SS.txt') as auth:
+        lines = list(auth)
+    auth_id = lines[3].rstrip()
+    auth_token = lines[4].rstrip()
+    return [auth_id, auth_token]
+
+
+def get_smart(address_list):
+    # Lookup address using SmartyStreets API
+    # modeled after:
+    # https://github.com/smartystreets/LiveAddressSamples/blob/master/python/street-address.py
+    print 'GET_SMART'
+    auth_list = read_auth()
+    location = 'https://api.smartystreets.com/street-address'
+    #location = 'https://api.smartystreets.com/lookup'
+    query_string = urllib.urlencode({
+    "auth-id": auth_list[0],
+    "auth-token": auth_list[1],
+    "street": address_list[0],
+    "city": address_list[1],
+    "state": address_list[2]
+    })
+    # what is this for: candidates=10'
+
+    url = location + "?" + query_string
+    print url
+
+    response = urllib.urlopen(url).read()
+    structure = json.loads(response)
+    pprint.pprint(structure)
+
+
+
+
+"""
+    creative_success = True
+    try:
+        clThings.raise_for_status()
+        print "Tried"
+    except requests.exceptions.HTTPError as e:
+        creative_success = False
+        print
+        print "##### HANDLED EXCEPTION #####"
+        print "cltampa.com error = ", e
+
+    # Only have cl content to process if request succeeded
+    if creative_success:
+
 
 def reverse_date(date_in):
     month = date_in[0:2]
@@ -155,7 +225,7 @@ def block_maker(layout_a):
                     address_string = inmate_table['Address'] + ', ' + inmate_table['City'] \
                         + ', FL' + '\n'
 
-                    OUTPUT.write(address_string)
+                    #OUTPUT.write(address_string)
 
 
             # SOID is always in last line of an entry. This pulls the data based on
@@ -253,10 +323,18 @@ def block_maker(layout_a):
 
     #file.close()
 '''
+"""
 
 if __name__ == '__main__':
+    print 'MAIN'
+    address = ['7800 NEBRASKA AV N',
+               'TAMPA',
+               'FL']
+    get_smart(address)
+
+    """
 	with open('..\\data\\address.txt', 'w') as OUTPUT:
 	    block_maker('..\data\LAYOUT A FILES\\1 LAYOUT A CSV.csv')
-
+    """
 
 
